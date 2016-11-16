e7db74c75db42a11c0666a2ee5b85443a15e5031
antselley@gmail.com <antselley@gmail.com>
2016-11-16 23:45:52 +0200

    Many changes - time for a change

--- contact_form.php
+++ contact_form.php
@@ -5,8 +5,8 @@
 
 <?php
 $to = "antselley@gmail.com";
-$subject = "Contact Form Submission" . $GET["subject"];
-$txt = "From: " . $_GET["name"] . $_GET["message"];
+$subject = "Contact Form Submission" . $_GET["subject"];
+$txt = "From: " . $_GET["name"] . "   " . $_GET["message"] . "   " . $_GET["email"];
 $headers = "From: " . $_GET["email"] ;
 
 
@@ -16,7 +16,7 @@ mail($to,$subject,$txt,$headers);
 
 Thank you for reaching out.
 We will be in touch shortly.
-<a href="https://schoolsgrandchallenge.000webhostapp.com"> Return to homepage. </a>
+<a href="http://ambitiouscuriosity.org/"> Return to homepage. </a>
 
 </body>
 </html>
--- newsletter.php
+++ newsletter.php
@@ -6,9 +6,9 @@
 <?php
 $to = "antselley@gmail.com";
 $subject = "New Newsletter Subscriber";
-$txt = "Yay, a subscriber!" . $_GET["name"] . $_GET["EMAIL"];
+$txt = "Yay, a subscriber! " . $_GET["NAME"] . "  " . $_GET["EMAIL"];
 $headers = "From: " . $_GET["EMAIL"] . "\r\n" .
-"CC: somebodyelse@example.com";
+"CC: somebodyelse@exampleflydfldkjfd.com";
 
 mail($to,$subject,$txt,$headers);
 ?>
@@ -18,7 +18,7 @@ Thank you for subscribing!
 We look forward to having you with us on this exciting journey.
 As a part of our community - please do help us spread the word.
 We're particularly excited about teachers/students/parents who might be interested in helping to drive the movement towards real world learning.
-<a href="https://schoolsgrandchallenge.000webhostapp.com"> Return to homepage. </a>
+<a href="http://ambitiouscuriosity.org/"> Return to homepage. </a>
 
 
 </body>
